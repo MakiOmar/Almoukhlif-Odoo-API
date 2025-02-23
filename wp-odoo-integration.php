51813c539fefa33c3ff59daf19d564482a0fb206
makiomar <mo7amed.maki@gmail.com>
2025-02-23 12:21:31 +0200

    Upgrade

--- wp-odoo-integration.php
+++ wp-odoo-integration.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: WordPress/Odoo Integration
  * Description: Integrates WooCommerce with Odoo to validate stock before adding products to the cart.
- * Version: 1.182
+ * Version: 1.183
  * Author: Mohammad Omar
  *
  * @package Odod
