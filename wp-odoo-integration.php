9645115228696c41c29f75c2cde30a92646d044d
makiomar <mo7amed.maki@gmail.com>
2025-02-24 22:57:45 +0200

    Send discount percent only

--- wp-odoo-integration.php
+++ wp-odoo-integration.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: WordPress/Odoo Integration
  * Description: Integrates WooCommerce with Odoo to validate stock before adding products to the cart.
- * Version: 1.196
+ * Version: 1.197
  * Author: Mohammad Omar
  *
  * @package Odod
@@ -352,7 +352,7 @@ function item_gifts($item_id, $item, &$order_data, &$discount)
                         'name'            => $product_name,
                         'product_uom_qty' => $item['quantity'],
                         'price_unit'      => $final_price,
-                        'discount'        => $discount > 0 ? $product_price * ($discount / 100) : 0,
+                        'discount'        => $discount > 0 ? $discount : 0,
                     );
                 }
             }
@@ -485,7 +485,7 @@ function process_odoo_order($order_ids, &$orders_data, &$orders_temp, $update = 
                 'name'            => $item->get_name(),
                 'product_uom_qty' => $quantity,
                 'price_unit'      => $final_price,
-                'discount'        => $discount_percent > 0 ? $product->get_price() * ($discount_percent / 100) : 0,
+                'discount'        => $discount_percent > 0 ? $discount_percent : 0,
             );
             if ($item->get_total() < 1) {
                 $discount += $product->get_price();
